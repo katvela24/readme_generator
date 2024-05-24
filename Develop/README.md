8ae6ec55af37b6caca678e97350b79b097443ea9
katvela24 <156866399+katvela24@users.noreply.github.com>
2024-05-23 21:16:07 -0400

    added questions section and other input

---
+++ Develop/README.md
@@ -0,0 +1,14 @@
+ # README Generator
+    ## Description
+    This application generates a high-quality, professional README depending on a series of questions a user answers in their command line using Node.js
+    ## Table of Contents
+    TBD
+    ## Installation
+    The user should clone the repository from GitHub and shoud download Node, a file system, and inquirer module.
+    ## Usage
+    Use inquirer from the command line to answer questions about the project.
+    ## Contribution
+    This project was created by Katherine Velasco. Please create a pull request to add any contributions.
+    ## Tests
+    TBD
+    
--- Develop/index.js
+++ Develop/index.js
@@ -1,65 +1,119 @@
 // TODO: Include packages needed for this application
 // const inquirer = import("inquirer");
 import inquirer from "inquirer";
-// const fs = require("inquirer");
+import fs from "fs";
+
 // const markdownGenerator = require(`./utils/generateMarkdown`)
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt([
-      {
-        type: "input",
-        name: "title",
-        message: "What is the title of GitHub project?",
-        
-      },
+        {
+            type: "input",
+            name: "title",
+            message: "What is the title of GitHub project?",
 
-      {
-        type: "input",
-        name: "description",
-        message: "What is the description?",
-        
-      },
+        },
 
-      {
-        type: "input",
-        name: "installation",
-        message: "What are the installation instructions?",
-        
-      },
+        {
+            type: "input",
+            name: "description",
+            message: "What is the description?",
 
-      {
-        type: "input",
-        name: "usage_information",
-        message: "What is the usage information?",
-        
-      },
+        },
 
-      {
-        type: "input",
-        name: "contribution_guidelines",
-        message: "What are the contribution guidelines?",
-        
-      },
+        {
+            type: "input",
+            name: "table_of_contents",
+            message: "What is the table of contents?",
+        },
 
-      {
+        {
+            type: "input",
+            name: "installation",
+            message: "What are the installation instructions?",
+
+        },
+
+        {
+            type: "input",
+            name: "usage_information",
+            message: "What is the usage information?",
+
+        },
+
+        {
+            type: "input",
+            name: "contribution_guidelines",
+            message: "What are the contribution guidelines?",
+
+        },
+
+        {
+            type: "input",
+            name: "test_instructions",
+            message: "What are the testing instructions?",
+
+        },
+
+        {
+            type: 'list',
+            name: 'license',
+            message: 'What license should your project have?',
+            choices: ['MIT', 'None'],
+        },
+
+        {
+            type: "input",
+            name: "username",
+            message: "What is your username?",
+
+        },
+
+    {
         type: "input",
-        name: "test_instructions",
-        message: "What are the test instructions",
-        
-      }
+        name: "email",
+        message: "What is your email address?",
+    },
+    
     ])
-    .then((response) =>
-      console.log(response)
+
+        .then((res) => {
+            console.log(res);
+            const readmeResults = createReadme(res);
+            writeToFile(readmeResults);
+        });
+}
+// Create a function to write README file
+function writeToFile(response) {
+    fs.writeFile('README.md', response, (err) => err ? console.error(err) : console.log('Success!')
     );
 
-
 }
+
+function createReadme(Answers) {
+
+    return ` # ${Answers.title}
+    ## Description
+    ${Answers.description}
+    ## Table of Contents
+    ${Answers.table_of_contents}
+    ## Installation
+    ${Answers.installation}
+    ## Usage
+    ${Answers.usage_information}
+    ## Contribution
+    ${Answers.contribution_guidelines}
+    ## Tests
+    ${Answers.test_instructions}
+    ## Questions
+    You can view more of my projects at https://github.com/${Answers.username}.
+    Contact me at ${Answers.email}.
+    `
+
+};
 // Function call to initialize app
 init();
